352c73131f291e0184c9c97bc8eca4932480d69e
NikoLesJ <3gdoma@gmail.com>
2024-10-27 17:43:23 +0100

    Fix server3

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://zwei.fun', 'https://your-frontend-url.onrender.com'],
+  origin: ['http://localhost:3000', 'https://zwei.fun'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
