b8d05af36a564b5cbdc6b9c6dbab9e47cfa69a16
NikoLesJ <3gdoma@gmail.com>
2024-10-27 17:30:23 +0100

    Fix server2

--- server.js
+++ server.js
@@ -1,38 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://zwei.fun'],
+  origin: ['http://localhost:3000', 'https://zwei.fun', 'https://your-frontend-url.onrender.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'], // Добавьте 'Authorization' если используете токены
-  credentials: true // Добавьте это, если вам нужно отправлять куки
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 };
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
+// Логирование запросов
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+
 // Import routes
-const productRoutes = require('./server/routes/productRoutes');
-const authRoutes = require('./server/routes/authRoutes');
-const categoryRoutes = require('./server/routes/categoryRoutes');
-const excelRoutes = require('./server/routes/excelRoutes');
-const hardcodedCategoryRoutes = require('./server/routes/hardcodedCategoryRoutes');
-const xmlRoutes = require('./server/routes/xmlRoutes');
+const productRoutes = require('./routes/productRoutes');
+const authRoutes = require('./routes/authRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
+const excelRoutes = require('./routes/excelRoutes');
+const hardcodedCategoryRoutes = require('./routes/hardcodedCategoryRoutes');
+const xmlRoutes = require('./routes/xmlRoutes');
 
-// После всех маршрутов
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Что-то пошло не так!');
-});
-
-app.get('/', (req, res) => {
-  res.send('Сервер работает');
-});
 // Use routes
 app.use('/api', productRoutes);
 app.use('/api', authRoutes);
@@ -41,11 +39,24 @@ app.use('/api', excelRoutes);
 app.use('/api', hardcodedCategoryRoutes);
 app.use('/api', xmlRoutes);
 
-app.use((req, res, next) => {
-  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
-  next();
+// Базовый маршрут для проверки работы сервера
+app.get('/api', (req, res) => {
+  res.send('API сервер работает');
 });
+
+// Обработка ошибок
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Что-то пошло не так!');
+});
+
+// Если вы хотите, чтобы сервер также обслуживал фронтенд:
+// app.use(express.static(path.join(__dirname, 'client/build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+// });
+
 // Start server
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Сервер запущен на порту ${port}`);
 });
